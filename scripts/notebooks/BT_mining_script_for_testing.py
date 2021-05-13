ce830e80c31692e5e8309d35d5c21cea74b97de8
unknown <razan.ghzouli@chalmers.se>
2021-05-13 11:26:32 +0200

    minor changes to naming file format

--- scripts/notebooks/BT_mining_script_for_testing.py
+++ scripts/notebooks/BT_mining_script_for_testing.py
@@ -16,6 +16,7 @@ from github import Github
 import pickle
 import sys
 import argparse
+from datetime import datetime
 
 def access_token(token):
     ACCESS_TOKEN = token ## enter your githun token
@@ -34,60 +35,70 @@ def access_token(token):
         else:
             print (e)
         
-## a function to query github by entering a specific string to search on files level
+### a function to query github by entering a specific string to search on files level
 def query_github(g, keywords = None):
     g= g 
     if keywords is None:
-        keywords = input('Enter keyword(s)[e.g py_trees_ros, main_tree_to_execute]: ')
+        keywords = input('Enter keyword(s)[e.g py_trees_ros, main_tree_to_execute] and file extension if needed. Separate using comma: ')
     else:
         keywords = keywords
     keywords = [keyword.strip() for keyword in keywords.split(',')]
-    query = f'"{keywords}" in:file extension:py' ### change extension to XML for "main_tree_to_execute"
+    if keywords[1] is None:
+         query = f'"{keywords}" in:file'
+    else: 
+        query = f'"{keywords[0]}" in:file extension:{keywords[1]}'
+        
     result = g.search_code(query, order='desc')
     print(f'Found {result.totalCount} file(s)')
     return result 
 
 
+###
 def extract_url_repo_name(result):
     # result = result
     new_repo = {}
     for file in result:
         url = file.download_url 
 
-        repo_name = file.repository.full_name.replace('/','_')
+        #repo_name = file.repository.full_name.replace('/','_')
+        repo_name = file.repository.full_name
     
         if repo_name in new_repo:
-        # append the new url to the existing array at this slot
+        ### append the new url to the existing array at this slot
             new_repo[repo_name].append(url)
         else:
-        # create a new array in this slot
+        ### create a new array in this slot
             new_repo[repo_name] = [url]
-            
-    f = open("repo_name_plus_URL.pkl","wb")
+    ### formating the file name to desired format with today's date
+    date = datetime.today().strftime('%d%m%Y')   
+    ### saving the found results 
+    f = open("repo_name_plus_URL_" +date+ ".pkl","wb")
     pickle.dump(new_repo,f)
     f.close()  
-    print("A file containing repo name and files URL was saved to your working directory")
+    print("A file containing repos names and files URL was saved to your working directory")
     print("------------------------------------------------------------------------------")
     print("The number of repository: {}".format(len(new_repo)))
     print("The number of files: {}".format(sum(len(dct) for dct in new_repo.values() )))
     
     return new_repo
 
+# #### uncomment this part and related part in main if you want to limit number of returned results
+# def limit_result_size(result, desired_size):    
+#     max_size = desired_size  
+#     if result.totalCount >= max_size:
+#         print(f'The number of mined GitHub files is {result.totalCount} and it was limmited to {max_size} ')
+#         return result[:max_size]
 
-def limit_result_size(result, desired_size):    
-    max_size = desired_size  
-    if result.totalCount >= max_size:
-        print(f'The number of mined GitHub files is {result.totalCount} and it was limmited to {max_size} ')
-        return result[:max_size]
-
-## remeber to call this function    
+### remeber to call this function    
 def download_models(save_path, url_repo_name_dict):
     file_count = 0
     for key,value in url_repo_name_dict.items():
         for i in range(len(value)):
             r = requests.get(value[i])
-            ## if you want to change the path where files are saved change  "/home/jovyan/src/notebooks/downloads/pytreeros" to where you want, also for XML fromat files change the re.search to xml instead of py 
-            open(os.path.join(save_path, os.path.basename(key+'_'+(re.search('/(\w*((%20\w)*)?.py)', value[i]).group(1)))) , "wb").write(r.content)
+            ### if you want to change the path where files are saved change  "/home/jovyan/src/notebooks/downloads/pytreeros" to where you want, also for XML fromat files change the re.search to xml instead of py 
+            # open(os.path.join(save_path, os.path.basename(key.replace('/','_')+'_'+(re.search('/(\w*((%20\w)*)?.py)', value[i]).group(1)))) , "wb").write(r.content)
+            open(os.path.join(save_path, os.path.basename(key.replace('/','_')+'_'+value.split("/")[-1])) , "wb").write(r.content)
+
             file_count += 1
 
     print("Download complete for {} files".format(file_count))
@@ -132,9 +143,9 @@ def main(accesstoken =None , query = None):
            
         result = query_github(g,keywords= None)
     
-    ## change here depending on number of files that you want to download. GitHub API has a limit of maximum 1000 files
-    desired_size = 100
-    result = limit_result_size(result, desired_size) 
+    ### change here depending on number of files that you want to download. GitHub API has a limit of maximum 1000 files
+    # desired_size = 1000
+    # result = limit_result_size(result, desired_size) 
    
     url_repo_name_dict= extract_url_repo_name(result)
     
